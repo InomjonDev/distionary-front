23f6c62ab34cf85d74c3220d7acedaded14d3ba4
InomjonDev <inomjonismanaliev@gmail.com>
2024-12-07 18:44:37 +0500

    api

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const API_URL = 'http://localhost:8000'
-// const API_URL = 'https://distionary-back-public.onrender.com'
+// const API_URL = 'http://localhost:8000'
+const API_URL = 'https://distionary-back-public.onrender.com'
 
 export const api = createApi({
 	reducerPath: 'api',
